396a1eea2155c9dd74f98f7abf59a299608a82f8
Hsi <its.hsichen@gmail.com>
2026-08-16 02:58:42 +0800

    chore: update sago-media to v0.5.0

--- Casks/sago-media.rb
+++ Casks/sago-media.rb
@@ -1,6 +1,6 @@
 cask "sago-media" do
-  version "0.4.1"
-  sha256 "3f190a272e357a77d0a4dd61483e9468b0d1a0439e23fa8fb1732e906098c1cc"
+  version "0.5.0"
+  sha256 "2add1fdbd950d09b5fae9311ec439ae4f1a0d27e34765719c7e7fd6b415a529a"
 
   url "https://github.com/Hsiii/sago-media-macos/releases/download/v#{version}/Sago-Media-#{version}.zip"
   name "Sago Media"
