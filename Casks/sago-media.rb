0d364ec2e4432fe40b12896969c5da3e49a363fd
Hsi <94444077+Hsiii@users.noreply.github.com>
2026-08-15 20:09:01 +0800

    chore: update sago-media to v0.2.0 (#4)
    Update the Sago Media cask to the notarized v0.2.0 release with Paste
    Files and Choose Files actions.\n\nVerified with brew style
    Casks/sago-media.rb.

--- Casks/sago-media.rb
+++ Casks/sago-media.rb
@@ -1,6 +1,6 @@
 cask "sago-media" do
-  version "0.1.0"
-  sha256 "a4a82b01bfdc96935aab9ec66c471692d55c62434303795a4ee7858651b83d75"
+  version "0.2.0"
+  sha256 "1da7449f8c74ec65e0d57c49d50742d1e2923a042f4e29bcf798c3c7d52da353"
 
   url "https://github.com/Hsiii/sago-media-macos/releases/download/v#{version}/Sago-Media-#{version}.zip"
   name "Sago Media"
