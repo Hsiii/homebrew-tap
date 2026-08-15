bfdbfe812dedaa248207c70b5cfa2943309b1829
Hsi <94444077+Hsiii@users.noreply.github.com>
2026-08-16 01:52:48 +0800

    chore: update sago-media to v0.4.1 (#9)
    Update the Sago Media cask to install the newly published v0.4.1
    release.
    
    - Bump the cask version to `0.4.1`.
    - Update the SHA-256 checksum to match the notarized release ZIP.

--- Casks/sago-media.rb
+++ Casks/sago-media.rb
@@ -1,6 +1,6 @@
 cask "sago-media" do
-  version "0.4.0"
-  sha256 "f4cdd3a45d83137fe87696a9599f18ed0badc764ff1e817d71909584c9bd2b91"
+  version "0.4.1"
+  sha256 "3f190a272e357a77d0a4dd61483e9468b0d1a0439e23fa8fb1732e906098c1cc"
 
   url "https://github.com/Hsiii/sago-media-macos/releases/download/v#{version}/Sago-Media-#{version}.zip"
   name "Sago Media"
