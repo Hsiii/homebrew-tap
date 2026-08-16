ee184bcd709b2391fa1fe7114338bab17b9ac973
Hsi <its.hsichen@gmail.com>
2026-08-16 23:55:14 +0800

    chore: migrate GitHub username to sago-cream

--- Casks/handytab.rb
+++ Casks/handytab.rb
@@ -2,10 +2,10 @@ cask "handytab" do
   version "0.1.0"
   sha256 "69f336531cb03d8123ae4012ba72a006b482c7bffc5bfe35f010af2f7064e2f5"
 
-  url "https://github.com/orangesago/handy-tab/releases/download/v#{version}/HandyTab-#{version}.zip"
+  url "https://github.com/sago-cream/handy-tab/releases/download/v#{version}/HandyTab-#{version}.zip"
   name "HandyTab"
   desc "Open a favorite browser tab with a hand wave or trackpad tap"
-  homepage "https://github.com/orangesago/handy-tab"
+  homepage "https://github.com/sago-cream/handy-tab"
 
   depends_on macos: :sonoma
 
